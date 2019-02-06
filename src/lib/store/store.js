2bc2a90238c75bec042a09821e67895ff9e66da2
Jesús Lizárraga <jesus@Jesuss-MBP.lan>
2019-02-06 12:40:03 -0600

    Add Redux Thunk

--- src/lib/store/store.js
+++ src/lib/store/store.js
@@ -1,7 +1,11 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import belloteroApp from './reducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk';
 
-const store = createStore(belloteroApp, composeWithDevTools());
+const store = createStore(
+    belloteroApp,
+    composeWithDevTools(applyMiddleware(thunk)),
+);
 
 export default store
